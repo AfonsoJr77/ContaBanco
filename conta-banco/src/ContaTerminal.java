4f6b0487ec36e23e049c8d292aeabfb86d6c7d84
Afonso Júnior <ti.afonsojunior@gmail.com>
2023-09-30 17:08:12 -0300

    simulador criação de conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite a agência: ");
+        int agencia = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Conta criada com sucesso!");
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque.");
+        
+        
+        
+         
+
+
+
+
+
+
+
     }
 }
